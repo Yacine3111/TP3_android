2e50e323f015b688ac3e2c5a60ed51393ade9a8e
YACINE BOUREZAK <2288656@cegepgranby.qc.ca>
2025-04-14 11:33:24 -0400

    loading recipes and delete

--- app/src/main/java/com/example/tp3/utils/RecipeStorage.java
+++ app/src/main/java/com/example/tp3/utils/RecipeStorage.java
@@ -2,17 +2,19 @@ package com.example.tp3.utils;
 
 import android.content.Context;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.example.tp3.models.Recipe;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -35,18 +37,12 @@ public class RecipeStorage {
         recipeDirectory= new File(context.getFilesDir(),RECIPES_DIR);
         recipeDirectory.mkdir();
 
-
     }
-
-
     public boolean saveRecipe(Recipe recipe) {
         String fileName = recipe.getId()+EXTENSION;
         try{
-
             File file=new File(recipeDirectory,fileName);
             file.createNewFile();
-
-
 
             FileOutputStream fos = new FileOutputStream(file);
             String recipeJson= gson.toJson(recipe);
@@ -64,27 +60,47 @@ public class RecipeStorage {
     public Recipe loadRecipe(UUID id) {
         String fileName= id+EXTENSION;
         try{
-            File file = new File(RECIPES_DIR,fileName);
 
+            File file = new File(recipeDirectory,fileName);
+            FileInputStream fis = new FileInputStream(file);
 
-            FileInputStream fis = context.openFileInput(fileName);
+            byte [] recipeByte  = new byte [(int)file.length()];
+            fis.read(recipeByte);
+           String recipe = new String(recipeByte,StandardCharsets.UTF_8);
+           fis.close();
+           return gson.fromJson(recipe,Recipe.class);
 
-            fis.close();
         } catch (FileNotFoundException e) {
             Log.d(TAG,String.format("Le fichier %s n'existe pas",fileName));
         }catch (IOException e){
             Log.d(TAG,String.format("Erreur en lisant dans le fichier %s",fileName));
         }
-
         return null;
     }
 
     public boolean deleteRecipe(UUID id) {
-        return false;
+        File file= new File(recipeDirectory,id+EXTENSION);
+        return file.delete();
     }
 
     public List<Recipe> getAllRecipes() {
+        File[] files = recipeDirectory.listFiles();
         List<Recipe> recipes = new ArrayList<>();
+        for(File file: files){
+            String fileName = file.getName();
+            try{
+                FileInputStream fis = new FileInputStream(file);
+                byte [] recipeByte  = new byte [(int)file.length()];
+                fis.read(recipeByte);
+                String recipe = new String(recipeByte, StandardCharsets.UTF_8);
+                fis.close();
+                recipes.add(gson.fromJson(recipe,Recipe.class));
+            } catch (FileNotFoundException e) {
+                Log.d(TAG,String.format("Le fichier %s n'existe pas",fileName));
+            } catch (IOException e) {
+                Log.d(TAG,String.format("Erreur en lisant dans le fichier %s",fileName));
+            }
+        }
         return recipes;
     }
 }
